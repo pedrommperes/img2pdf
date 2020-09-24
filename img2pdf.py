c5799001efff0cee2c064ab8931ecc39e5a6f29d
pedrommperes <48154354+pedrommperes@users.noreply.github.com>
2020-09-24 22:35:58 +0100

    First commit

---
+++ img2pdf.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import os
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+root = Tk()
+root.withdraw()
+filename = askopenfilename(title="Select file")
+root.update()
+root.destroy()
+
+image1 = Image.open(filename)
+im1 = image1.convert('RGB')
+
+base = os.path.basename(filename)
+base_ext = os.path.splitext(base)[0]
+#string = "r'" + dir_path + "\\" + base + ".pdf'"
+#print(string)
+im1.save(base_ext + ".pdf")
